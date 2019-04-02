6fdc2bcb96ff2a730b1adea82f229d7296f92878
shin1rok <skssikm@gmail.com>
2019-04-02 09:00:31 +0900

    exawizards2019/A/refactor.rb

--- exawizards2019/A/refactor.rb
+++ exawizards2019/A/refactor.rb
@@ -1,18 +1,1 @@
-gets
-a = gets.chomp.split.map(&:to_i)
-
-alice = 0
-bob   = 0
-
-a.size.times do |n|
-  if n.even?
-    alice += a.max
-  else
-    bob += a.max
-  end
-  a.delete_at(a.find_index(a.max))
-end
-
-puts alice - bob
-
-# 大きい順に並べ替えて順番に入れていく
+puts gets.split.uniq.size == 1 ? "Yes" : "No"
